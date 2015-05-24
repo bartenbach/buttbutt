0b050e3ca8d08c53cbb046e70a06295790bf16b6
Klong <klong@klong.nu>
2015-05-24 05:55:59 +0200

    Fixing a bug where NullPointerException is thrown if BotResponse.getChannel is called on private messages

--- src/main/java/net/alureon/ircbutt/handler/command/WakeRoomHandler.java
+++ src/main/java/net/alureon/ircbutt/handler/command/WakeRoomHandler.java
@@ -1,6 +1,7 @@
 package net.alureon.ircbutt.handler.command;
 
 import net.alureon.ircbutt.BotResponse;
+import org.pircbotx.Channel;
 import org.pircbotx.User;
 
 /**
@@ -9,13 +10,16 @@ import org.pircbotx.User;
 public class WakeRoomHandler {
 
     public void handleWakeRoom(BotResponse response) {
-        StringBuilder sb = new StringBuilder();
-        for (User user : response.getChannel().getUsers()) {
-            if (sb.length() > 0) {
-                sb.append(' ');
+        Channel channel = response.getChannel();
+        if (channel != null) {
+            StringBuilder sb = new StringBuilder();
+            for (User user : channel.getUsers()) {
+                if (sb.length() > 0) {
+                    sb.append(' ');
+                }
+                sb.append(user.getNick());
             }
-            sb.append(user.getNick());
+            response.chat(sb.toString());
         }
-        response.chat(sb.toString());
     }
 }
