39b62f277f0e7aa371cfeeed501ed86ff9fd24b3
Blake Bartenbach <blakebartenbach@gmail.com>
2017-12-15 09:17:57 -0600

    Added many more cryptocurrencies.

--- src/main/java/net/alureon/ircbutt/command/commands/cryptocurrency/CryptocurrencyCommand.java
+++ src/main/java/net/alureon/ircbutt/command/commands/cryptocurrency/CryptocurrencyCommand.java
@@ -31,65 +31,71 @@ public final class CryptocurrencyCommand implements Command {
     @Override
     public BotResponse executeCommand(final IRCbutt butt, final GenericMessageEvent event, final String[] cmd) {
         String url = "";
-        Class cryptocurrency = null;
-        if (cmd[0].startsWith("btc")) {
-            url = "https://api.coinbase.com/v2/prices/BTC-USD/spot";
-            cryptocurrency = BTC.class;
-        } else if (cmd[0].startsWith("ltc")) {
-            url = "https://api.coinbase.com/v2/prices/LTC-USD/spot";
-            cryptocurrency = LTC.class;
-        } else if (cmd[0].startsWith("eth")) {
-            url = "https://api.coinbase.com/v2/prices/ETH-USD/spot";
-            cryptocurrency = ETH.class;
-        } else if (cmd[0].startsWith("vtc") || cmd[0].startsWith("xrp") || cmd[0].startsWith("bch")
-                || cmd[0].startsWith("dash") || cmd[0].startsWith("iota")) {
-            return handleCoinMarketRequest(cmd[0]);
-        }
-        try (InputStream is = new URL(url).openStream()) {
-            BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(is, Charset.forName("UTF-8")));
-            String jsonText = readAll(bufferedReader);
-            Gson gson = new Gson();
-            Cryptocurrency currency = gson.fromJson(jsonText, (Type) cryptocurrency);
-            if (!cmd[0].endsWith("v")) {
-                return new BotResponse(BotIntention.CHAT, null, currency.getData().getBase()
-                        + ": " + currency.getData().getAmount() + " " + currency.getData().getCurrency());
-            } else {
-                return new BotResponse(BotIntention.CHAT, null, currency.getData().getAmount());
-            }
-        } catch (IOException e) {
-            log.error("Could not get market status! ", e.getMessage());
-        }
-        return null;
-}
-
-    /**
-     * Handles a request to CoinMarketCap's API as opposed to GDAX.
-     * Very few cryptocurrencyies are supported by GDAX/Coinbase currently.
-     * @param command The command that was passed (may be a value request).
-     * @return The bot's response (the ticker price)
-     */
-    @SuppressWarnings("unchecked") // this is from the cast into a CoinMarketCapResponse object.
-    private BotResponse handleCoinMarketRequest(final String command) {
-        String url = "";
-        if (command.startsWith("vtc")) {
+        if (cmd[0].startsWith("vtc")) {
             url = "https://api.coinmarketcap.com/v1/ticker/vertcoin/";
-        } else if (command.startsWith("xrp")) {
+        } else if (cmd[0].startsWith("btc")) {
+            url = "https://api.coinmarketcap.com/v1/ticker/bitcoin/";
+        } else if (cmd[0].startsWith("eth")) {
+            url = "https://api.coinmarketcap.com/v1/ticker/ethereum/";
+        } else if (cmd[0].startsWith("ltc")) {
+            url = "https://api.coinmarketcap.com/v1/ticker/litecoin/";
+        } else if (cmd[0].startsWith("wtc")) {
+            url = "https://api.coinmarketcap.com/v1/ticker/walton/";
+        } else if (cmd[0].startsWith("xrp")) {
             url = "https://api.coinmarketcap.com/v1/ticker/ripple/";
-        } else if (command.startsWith("bch")) {
+        } else if (cmd[0].startsWith("bch")) {
             url = "https://api.coinmarketcap.com/v1/ticker/bitcoin-cash/";
-        } else if (command.startsWith("dash")) {
+        } else if (cmd[0].startsWith("dash")) {
             url = "https://api.coinmarketcap.com/v1/ticker/dash/";
-        } else if (command.startsWith("iota")) {
+        } else if (cmd[0].startsWith("iota")) {
             url = "https://api.coinmarketcap.com/v1/ticker/iota/";
+        } else if (cmd[0].startsWith("ada")) {
+            url = "https://api.coinmarketcap.com/v1/ticker/cardano/";
+        } else if (cmd[0].startsWith("xem")) {
+            url = "https://api.coinmarketcap.com/v1/ticker/nem/";
+        } else if (cmd[0].startsWith("btg")) {
+            url = "https://api.coinmarketcap.com/v1/ticker/bitcoin-gold/";
+        } else if (cmd[0].startsWith("xmr")) {
+            url = "https://api.coinmarketcap.com/v1/ticker/monero/";
+        } else if (cmd[0].startsWith("eos")) {
+            url = "https://api.coinmarketcap.com/v1/ticker/eos/";
+        } else if (cmd[0].startsWith("xlm")) {
+            url = "https://api.coinmarketcap.com/v1/ticker/stellar/";
+        } else if (cmd[0].startsWith("zec")) {
+            url = "https://api.coinmarketcap.com/v1/ticker/zcash/";
+        } else if (cmd[0].startsWith("usdt")) {
+            url = "https://api.coinmarketcap.com/v1/ticker/tether/";
+        } else if (cmd[0].startsWith("steem")) {
+            url = "https://api.coinmarketcap.com/v1/ticker/steem/";
+        } else if (cmd[0].startsWith("doge")) {
+            url = "https://api.coinmarketcap.com/v1/ticker/dogecoin/";
+        } else if (cmd[0].startsWith("bnb")) {
+            url = "https://api.coinmarketcap.com/v1/ticker/binance-coin/";
+        } else if (cmd[0].startsWith("gnt")) {
+            url = "https://api.coinmarketcap.com/v1/ticker/golem/";
+        } else if (cmd[0].startsWith("etc")) {
+            url = "https://api.coinmarketcap.com/v1/ticker/ethereum-classic/";
+        } else if (cmd[0].startsWith("neo")) {
+            url = "https://api.coinmarketcap.com/v1/ticker/neo/";
+        } else if (cmd[0].startsWith("ppt")) {
+            url = "https://api.coinmarketcap.com/v1/ticker/populous/";
+        } else if (cmd[0].startsWith("bcc")) {
+            url = "https://api.coinmarketcap.com/v1/ticker/bitconnect/";
+        } else if (cmd[0].startsWith("qtum")) {
+            url = "https://api.coinmarketcap.com/v1/ticker/qtum/";
+        } else if (cmd[0].startsWith("waves")) {
+            url = "https://api.coinmarketcap.com/v1/ticker/waves/";
         }
         try (InputStream is = new URL(url).openStream()) {
             BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(is, Charset.forName("UTF-8")));
             String jsonText = readAll(bufferedReader);
-            Type currencyType = new TypeToken<List<CoinMarketCapResponse>>() { }.getType();
+            Type currencyType = new TypeToken<List<CoinMarketCapResponse>>() {
+            }.getType();
             List<CoinMarketCapResponse> currency = new Gson().fromJson(jsonText, currencyType);
-            if (!command.endsWith("v")) {
+            if (!cmd[0].endsWith("v")) {
                 return new BotResponse(BotIntention.CHAT, null, currency.get(0).getName()
-                        + ": " + currency.get(0).getPriceUsd() + " [" + currency.get(0).getPercentChange24h() + "]");
+                        + ": " + currency.get(0).getPriceUsd() + " [" + currency.get(0).getPercentChange24h() + "%] "
+                        + " Rank: " + currency.get(0).getRank());
             } else {
                 return new BotResponse(BotIntention.CHAT, null, currency.get(0).getPriceUsd());
             }
@@ -118,7 +124,10 @@ public final class CryptocurrencyCommand implements Command {
     @Override
     public ArrayList<String> getCommandAliases() {
         return new ArrayList<>(Arrays.asList("btc", "ltc", "eth", "btcv", "ltcv", "ethv", "vtc", "vtcv", "xrp", "xrpv",
-                "bch", "bchv", "dash", "dashv", "iota", "iotav"));
+                "bch", "bchv", "dash", "dashv", "iota", "iotav", "wtc", "wtcv", "ada", "adav", "xem", "xemv", "btg",
+                "btgv", "xmr", "xmrv", "eos", "eosv", "xlm", "xlmv", "zec", "zecv", "usdt", "usdtv", "steem", "steemv",
+                "doge", "dogev", "bnb", "bnbv", "gnt", "gntv", "etc", "etcv", "neo", "neov", "ppt", "pptv", "bcc",
+                "bccv", "qtum", "qtumv", "waves", "wavesv"));
 
     }
 
