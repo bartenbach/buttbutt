8ed34b826344955455b7184783c7ce08d77c6f64
Tyler Zoucha <tylerzoucha@gmail.com>
2017-12-16 15:15:55 -0600

    Updated broken Amazon search

--- src/main/java/net/alureon/ircbutt/command/commands/amazon/AmazonSearchCommand.java
+++ src/main/java/net/alureon/ircbutt/command/commands/amazon/AmazonSearchCommand.java
@@ -9,7 +9,6 @@ import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.jsoup.Connection;
 import org.jsoup.Jsoup;
-import org.jsoup.nodes.Attributes;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
@@ -56,7 +55,7 @@ public final class AmazonSearchCommand implements Command {
                     String urlText = URLDecoder.decode(result.text(), "utf8");
                     response = new BotResponse(BotIntention.CHAT, null, urlText);
                 } else {
-                    butt.getCommandHandler().addMore(URLDecoder.decode(result.text(), "utf-8");
+                    butt.getCommandHandler().addMore(URLDecoder.decode(result.text(), "utf-8"));
                 }
             }
         } catch (IOException e) {
